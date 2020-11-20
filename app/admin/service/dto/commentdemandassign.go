cbe9bd3bff5cc7ac3a850978f78519276590bf5f
C.W.T <woshiniama@vip.qq.com>
2020-11-20 23:08:11 +0800

    -

--- app/admin/service/dto/commentdemandassign.go
+++ app/admin/service/dto/commentdemandassign.go
@@ -4,24 +4,22 @@ import (
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 
-	"go-admin/pkg/models"
 	"go-admin/common/dto"
 	"go-admin/common/log"
 	common "go-admin/common/models"
+	"go-admin/pkg/models"
 	"go-admin/tools"
 )
 
 type CommentDemandAssignSearch struct {
-	dto.Pagination     `search:"-"`
-    DemandSerial string `form:"demandSerial" search:"type:exact;column:demand_serial;table:comment_demand_assign" comment:"需求编号"`
+	dto.Pagination `search:"-"`
+	Serial         string `form:"serial" search:"type:exact;column:serial;table:comment_demand_assign" comment:"任务编号"`
 
-    Commentator string `form:"commentator" search:"type:exact;column:commentator;table:comment_demand_assign" comment:"测评人"`
+	DemandSerial string `form:"demandSerial" search:"type:exact;column:demand_serial;table:comment_demand_assign" comment:"需求编号"`
 
-    CommentAccount string `form:"commentAccount" search:"type:exact;column:comment_account;table:comment_demand_assign" comment:"测评账号"`
+	Commentator string `form:"commentator" search:"type:exact;column:commentator;table:comment_demand_assign" comment:"测评人"`
 
-    Status string `form:"status" search:"type:exact;column:status;table:comment_demand_assign" comment:"状态"`
-
-    
+	Status string `form:"status" search:"type:exact;column:status;table:comment_demand_assign" comment:"状态"`
 }
 
 func (m *CommentDemandAssignSearch) GetNeedSearch() interface{} {
@@ -29,12 +27,12 @@ func (m *CommentDemandAssignSearch) GetNeedSearch() interface{} {
 }
 
 func (m *CommentDemandAssignSearch) Bind(ctx *gin.Context) error {
-    msgID := tools.GenerateMsgIDFromContext(ctx)
-    err := ctx.ShouldBind(m)
-    if err != nil {
-    	log.Debugf("MsgID[%s] ShouldBind error: %s", msgID, err.Error())
-    }
-    return err
+	msgID := tools.GenerateMsgIDFromContext(ctx)
+	err := ctx.ShouldBind(m)
+	if err != nil {
+		log.Debugf("MsgID[%s] ShouldBind error: %s", msgID, err.Error())
+	}
+	return err
 }
 
 func (m *CommentDemandAssignSearch) Generate() dto.Index {
@@ -43,64 +41,51 @@ func (m *CommentDemandAssignSearch) Generate() dto.Index {
 }
 
 type CommentDemandAssignControl struct {
-    
-    ID uint `uri:"ID" comment:""` // 
+	ID uint `uri:"ID" comment:""` //
 
-    DemandSerial string `json:"demandSerial" comment:"需求编号"`
-    
+	Serial string `json:"serial" comment:"任务编号"`
 
-    DemandSnapshotCode string `json:"demandSnapshotCode" comment:"快照编号"`
-    
+	DemandSerial string `json:"demandSerial" comment:"需求编号"`
 
-    DeptId string `json:"deptId" comment:"任务所属企业ID"`
-    
+	DemandSnapshotCode string `json:"demandSnapshotCode" comment:"快照ID"`
 
-    Commentator string `json:"commentator" comment:"测评人"`
-    
+	DeptId string `json:"deptId" comment:"企业"`
 
-    CommentAccount string `json:"commentAccount" comment:"测评账号"`
-    
+	Commentator string `json:"commentator" comment:"测评人"`
 
-    CommentName string `json:"commentName" comment:"测评账号名称"`
-    
+	CommentAccount string `json:"commentAccount" comment:"测评账号"`
 
-    Status string `json:"status" comment:"状态"`
-    
+	CommentName string `json:"commentName" comment:"测评账号名称"`
 
-    TipsStatus string `json:"tipsStatus" comment:"提示"`
-    
+	Status string `json:"status" comment:"状态"`
 
-    IncomeAccouunt string `json:"incomeAccouunt" comment:"收款账号"`
-    
+	TipsStatus string `json:"tipsStatus" comment:"提示"`
 
-    IncomeType string `json:"incomeType" comment:"收款账号类型"`
-    
+	IncomeAccount string `json:"incomeAccount" comment:"收款账号"`
 
-    IncomeName string `json:"incomeName" comment:"收款名称"`
-    
+	IncomeType string `json:"incomeType" comment:"收款类型"`
 
-    OrderMedias string `json:"orderMedias" comment:"下单截图"`
-    
+	IncomeName string `json:"incomeName" comment:"收款名称"`
 
-    CommentMedias string `json:"commentMedias" comment:"留评截图"`
-    
+	OrderMedias string `json:"orderMedias" comment:"下单截图"`
 
-    SettleMedias string `json:"settleMedias" comment:"结算截图"`
-    
+	CommentMedias string `json:"commentMedias" comment:"留评截图"`
+
+	SettleMedias string `json:"settleMedias" comment:"结算截图"`
 }
 
 func (s *CommentDemandAssignControl) Bind(ctx *gin.Context) error {
-    msgID := tools.GenerateMsgIDFromContext(ctx)
-    err := ctx.ShouldBindUri(s)
-    if err != nil {
-        log.Debugf("MsgID[%s] ShouldBindUri error: %s", msgID, err.Error())
-        return err
-    }
-    err = ctx.ShouldBind(s)
-    if err != nil {
-        log.Debugf("MsgID[%s] ShouldBind error: %#v", msgID, err.Error())
-    }
-    return err
+	msgID := tools.GenerateMsgIDFromContext(ctx)
+	err := ctx.ShouldBindUri(s)
+	if err != nil {
+		log.Debugf("MsgID[%s] ShouldBindUri error: %s", msgID, err.Error())
+		return err
+	}
+	err = ctx.ShouldBind(s)
+	if err != nil {
+		log.Debugf("MsgID[%s] ShouldBind error: %#v", msgID, err.Error())
+	}
+	return err
 }
 
 func (s *CommentDemandAssignControl) Generate() dto.Control {
@@ -110,22 +95,23 @@ func (s *CommentDemandAssignControl) Generate() dto.Control {
 
 func (s *CommentDemandAssignControl) GenerateM() (common.ActiveRecord, error) {
 	return &models.CommentDemandAssign{
-	
-        Model:     gorm.Model{ID: s.ID},
-        DemandSerial:  s.DemandSerial,
-        DemandSnapshotCode:  s.DemandSnapshotCode,
-        DeptId:  s.DeptId,
-        Commentator:  s.Commentator,
-        CommentAccount:  s.CommentAccount,
-        CommentName:  s.CommentName,
-        Status:  s.Status,
-        TipsStatus:  s.TipsStatus,
-        IncomeAccouunt:  s.IncomeAccouunt,
-        IncomeType:  s.IncomeType,
-        IncomeName:  s.IncomeName,
-        OrderMedias:  s.OrderMedias,
-        CommentMedias:  s.CommentMedias,
-        SettleMedias:  s.SettleMedias,
+
+		Model:              gorm.Model{ID: s.ID},
+		Serial:             s.Serial,
+		DemandSerial:       s.DemandSerial,
+		DemandSnapshotCode: s.DemandSnapshotCode,
+		DeptId:             s.DeptId,
+		Commentator:        s.Commentator,
+		CommentAccount:     s.CommentAccount,
+		CommentName:        s.CommentName,
+		Status:             s.Status,
+		TipsStatus:         s.TipsStatus,
+		IncomeAccount:      s.IncomeAccount,
+		IncomeType:         s.IncomeType,
+		IncomeName:         s.IncomeName,
+		OrderMedias:        s.OrderMedias,
+		CommentMedias:      s.CommentMedias,
+		SettleMedias:       s.SettleMedias,
 	}, nil
 }
 
--- pkg/models/commentdemandassign.go
+++ pkg/models/commentdemandassign.go
@@ -1,33 +1,34 @@
 package models
 
 import (
-    "gorm.io/gorm"
+	"gorm.io/gorm"
 
 	"go-admin/common/models"
 )
 
 type CommentDemandAssign struct {
-    gorm.Model
-    models.ControlBy
-    
-    DemandSerial string `json:"demandSerial" gorm:"type:varchar(64);comment:需求编号"` // 
-    DemandSnapshotCode string `json:"demandSnapshotCode" gorm:"type:varchar(32);comment:快照编号"` // 
-    DeptId string `json:"deptId" gorm:"type:bigint;comment:任务所属企业ID"` // 
-    Commentator string `json:"commentator" gorm:"type:varchar(16);comment:测评人"` // 
-    CommentAccount string `json:"commentAccount" gorm:"type:varchar(32);comment:测评账号"` // 
-    CommentName string `json:"commentName" gorm:"type:varchar(32);comment:测评账号名称"` // 
-    Status string `json:"status" gorm:"type:int;comment:状态"` // 
-    TipsStatus string `json:"tipsStatus" gorm:"type:int;comment:提示"` // 
-    IncomeAccouunt string `json:"incomeAccouunt" gorm:"type:varchar(64);comment:收款账号"` // 
-    IncomeType string `json:"incomeType" gorm:"type:int;comment:收款账号类型"` // 
-    IncomeName string `json:"incomeName" gorm:"type:varchar(64);comment:收款名称"` // 
-    OrderMedias string `json:"orderMedias" gorm:"type:varchar(32);comment:下单截图"` // 
-    CommentMedias string `json:"commentMedias" gorm:"type:varchar(32);comment:留评截图"` // 
-    SettleMedias string `json:"settleMedias" gorm:"type:varchar(32);comment:结算截图"` // 
+	gorm.Model
+	models.ControlBy
+
+	Serial             string `json:"serial" gorm:"type:varchar(64);comment:任务编号"`             //
+	DemandSerial       string `json:"demandSerial" gorm:"type:varchar(64);comment:需求编号"`       //
+	DemandSnapshotCode string `json:"demandSnapshotCode" gorm:"type:varchar(32);comment:快照ID"` //
+	DeptId             string `json:"deptId" gorm:"type:bigint;comment:企业"`                    //
+	Commentator        string `json:"commentator" gorm:"type:varchar(16);comment:测评人"`         //
+	CommentAccount     string `json:"commentAccount" gorm:"type:varchar(32);comment:测评账号"`     //
+	CommentName        string `json:"commentName" gorm:"type:varchar(32);comment:测评账号名称"`      //
+	Status             string `json:"status" gorm:"type:int;comment:状态"`                       //
+	TipsStatus         string `json:"tipsStatus" gorm:"type:int;comment:提示"`                   //
+	IncomeAccount      string `json:"incomeAccount" gorm:"type:varchar(64);comment:收款账号"`      //
+	IncomeType         string `json:"incomeType" gorm:"type:int;comment:收款类型"`                 //
+	IncomeName         string `json:"incomeName" gorm:"type:varchar(64);comment:收款名称"`         //
+	OrderMedias        string `json:"orderMedias" gorm:"type:varchar(32);comment:下单截图"`        //
+	CommentMedias      string `json:"commentMedias" gorm:"type:varchar(32);comment:留评截图"`      //
+	SettleMedias       string `json:"settleMedias" gorm:"type:varchar(32);comment:结算截图"`       //
 }
 
 func (CommentDemandAssign) TableName() string {
-    return "comment_demand_assign"
+	return "comment_demand_assign"
 }
 
 func (e *CommentDemandAssign) Generate() models.ActiveRecord {
